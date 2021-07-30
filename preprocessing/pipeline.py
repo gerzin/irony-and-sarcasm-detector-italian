8e8f5394c57ce2373a83e7da12c3369e34409d94
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2021-07-30 17:46:32 +0200

    added hashtags extraction step

--- preprocessing/pipeline.py
+++ preprocessing/pipeline.py
@@ -1,22 +1,46 @@
-from sklearn.pipeline import Pipeline
-import re
+import pandas as pd
+import datautils
 
 
-def remove_urls(s: str, sub='', strip=True):
+def remove_urls(df, col_name='text'):
     """
-    Removes urls from strings.
-    :param s: String to clean.
-    :param sub: What to substitute the urls with (default = '').
-    :param strip: If True the returned string is stripped. (default = True)
-    :return: The string obtained by replacing the urls in the original string with the value of the sub parameter.
+    Removes the URLs from a column.
+
+    :param df: dataframe to clean.
+    :param col_name: column to remove urls from.
+    :return: The dataframe without URLs in col_name.
     """
-    cleaned = re.sub(r"http\S+", sub, s)
-    return cleaned.strip() if strip else cleaned
+    df[col_name] = df[col_name].apply(datautils.remove_urls)
+    return df
+
+
+def extract_hashtags(df, col_name='text'):
+    """
+    Augments the dataframe with a column containing a list of hashtags present in the col_name column.
+
+    :param df: dataframe to augment.
+    :param col_name: column to look up hashtags from.
+    :return: The augmented dataframe containing a column for the hashtags.
+    """
+    df['hashtags'] = df[col_name].apply(datautils.find_hashtags)
+    return df
 
 
 class ItalianTweetsPreprocessingPipeline:
+    def __init__(self, transformations=None):
+        if transformations is None:
+            transformations = [
+                remove_urls,
+                extract_hashtags
+            ]
+        self.transformations = transformations
 
-    def __init__(self, *args, **kwargs):
-        self.pipeline = Pipeline(
-
-        )
+    def apply(self, df: pd.DataFrame):
+        """
+        Run a dataframe through the pipeline's steps.
+        :param df: dataframe to process.
+        :return: the transformed dataframe.
+        """
+        return (df.pipe(self.transformations[0])
+                .pipe(self.transformations[1])
+                )
