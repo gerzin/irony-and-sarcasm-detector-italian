18c1d80bcd6a27fe77590b02f350268ce0d645af
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2021-08-06 17:29:14 +0200

    tok

--- preprocessing/tokenization.py
+++ preprocessing/tokenization.py
@@ -5,6 +5,9 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pandas as pd
 from config import Config
 
+OOV_TOKEN="<OOV>"
+TOKENIZER_NUM_WORDS=None
+
 
 def tokenize_frame(tweets):
     """
@@ -13,7 +16,7 @@ def tokenize_frame(tweets):
     :param tweets: column of the dataframe containing the tweets
     :return: a DataFrame of sequences ready to be fed to a neural network
     """
-    tokenizer = Tokenizer(oov_token="<OOV>")
+    tokenizer = Tokenizer(oov_token=OOV_TOKEN)
     tokenizer.fit_on_texts(tweets)
 
     set_stopwords = stopwords.words(Config.TEXT_LANGUAGE)
@@ -35,3 +38,27 @@ def tokenize_frame(tweets):
 
     # return new_frame
     return padded
+
+
+class ItalianTweetsTokenizer:
+    def __init__(self, preprocessing_pipeline=None, tokenizer=None):
+        self.preprocessing_pipeline = preprocessing_pipeline
+        self.tokenizer = tokenizer
+
+    def tokenize(self, df, col_name='text'):
+        if self.preprocessing_pipeline:
+            df = self.preprocessing_pipeline.apply(df)
+        if not self.tokenizer:
+            tok = Tokenizer(oov_token=OOV_TOKEN, num_words=TOKENIZER_NUM_WORDS)
+            tok.fit_on_texts(df[col_name])
+            self.tokenizer = tok
+
+        stop_words_set = set(stopwords.words(Config.TEXT_LANGUAGE))
+        tweets = df[col_name].tolist()
+        tokenized = [list(filter(lambda x: not x in stop_words_set), tweet) for tweet in tweets]
+
+
+
+
+
+
