b74b6a9a6edcc5d6a068f9ef49ea6c8b6d7d8dde
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2021-09-29 18:53:35 +0200

    fixed splitting

--- models/utils/model_selection.py
+++ models/utils/model_selection.py
@@ -1,7 +1,7 @@
 from sklearn.model_selection import train_test_split
 
 
-def train_test_validation_split(df, train_split=0.8, val_split=0.1, test_split=0.1, seed=42):
+def train_test_validation_split(df, train_split=0.8, seed=42):
     """
     Splits the tweets dataset into train, test and validation sets.
 
@@ -14,12 +14,10 @@ def train_test_validation_split(df, train_split=0.8, val_split=0.1, test_split=0
     :param seed: seed for the rng.
     :return: (X_train, y_train), (X_test, y_test), (X_val, y_val)
     """
-    assert (train_split + test_split + val_split) == 1
-    assert (val_split == test_split) # we want equal-sized sets
 
     features = df['text']
     targets = df[['irony', 'sarcasm']]
 
     X_train, X, y_train, y = train_test_split(features, targets, train_size=train_split, random_state=seed)
-    X_test, X_val, y_test, y_val = train_test_split(X, y, train_size=test_split, random_state=seed)
+    X_test, X_val, y_test, y_val = train_test_split(X, y, train_size=0.5, random_state=seed)
     return (X_train, y_train), (X_test, y_test), (X_val, y_val)
